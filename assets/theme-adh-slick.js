a2a22edb4f0cca7aa35206f92188e4bea12d84c5
Your Name <tomasbuitrago6@gmail.com>
2024-02-09 14:42:08 -0500

    first commit

---
+++ assets/theme-adh-slick.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function () {
+/**
+ * Add inside here all slick instances
+ */
+//   $('#highlight_products_slider').slick({
+//       responsive: [
+//           {
+//             breakpoint: 1024,
+//             settings: {
+//               slidesToShow: 3,
+//               slidesToScroll: 3,
+//             }
+//           },
+//           {
+//             breakpoint: 768,
+//             settings: {
+//               slidesToShow: 2,
+//               slidesToScroll: 2
+//             }
+//           },
+//           {
+//             breakpoint: 425,
+//             settings: {
+//               slidesToShow: 1,
+//               slidesToScroll: 1
+//             }
+//           }
+//         ]
+//   });
+})
